045b4c081376145a4e0598fc132a54c17f8ee6ae
Nahuel <nahuelmartinez390@gmail.com>
2024-07-25 10:38:21 -0300

    saca carrusel marcas

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -94,7 +94,7 @@ export default function Product() {
                     ))}
                 </div>
             </section>
-            <BrandCarousel />
+            {/* <BrandCarousel /> */}
         </>
     );
 }
